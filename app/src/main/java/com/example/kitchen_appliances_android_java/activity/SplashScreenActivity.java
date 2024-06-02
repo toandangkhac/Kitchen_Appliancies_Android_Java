7d60a0277579586add6e5a1a0637de4107387369
toandangkhac <toandang1704@gmail.com>
2024-06-02 12:44:31 +0700

    completed login function

--- app/src/main/java/com/example/kitchen_appliances_android_java/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/kitchen_appliances_android_java/activity/SplashScreenActivity.java
@@ -8,6 +8,7 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -17,10 +18,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BaseHttpStack;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.kitchen_appliances_android_java.R;
+import com.example.kitchen_appliances_android_java.api.TrustAllCertificatesSSLSocketFactory;
+import com.example.kitchen_appliances_android_java.model.Token;
+import com.example.kitchen_appliances_android_java.model.TokenResponse;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+    private HurlStack hurlStack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,23 +55,66 @@ public class SplashScreenActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        hurlStack = new HurlStack() {
+            @Override
+            protected HttpURLConnection createConnection(URL url) throws IOException {
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) super.createConnection(url);
+                try {
+                    httpsURLConnection.setSSLSocketFactory(new TrustAllCertificatesSSLSocketFactory(null));
+                    httpsURLConnection.setHostnameVerifier(new HostnameVerifier() {
+                        @Override
+                        public boolean verify(String hostname, SSLSession session) {
+                            // Ignore host name verification. It returns true for all host names.
+                            return true;
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return httpsURLConnection;
+            }
+        };
         // Delay for 1 second before loading user data
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                String url = "https://10.0.2.2:7161/api/Account/login";
                 // Check login status
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(SplashScreenActivity.this);
-                boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
-
-                // Start MainActivity if the user is logged in, otherwise start LoginSignUpActivity
-                Intent intent;
-                if (isLoggedIn) {
-                    intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                } else {
-                    intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+                String email = sharedPreferences.getString("email", "");
+                String password = sharedPreferences.getString("password", "");
+                JSONObject params = new JSONObject();
+                try {
+                    params.put("email", email);
+                    params.put("password", password);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                startActivity(intent);
-                finish();
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Intent intent = new Intent(SplashScreenActivity.this, LoginSignUpActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+
+
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext(), hurlStack);
+                queue.add(jsonObjectRequest);
+
+
             }
         }, 2000); // Delay for 1 second
     }
