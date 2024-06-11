7d24839adc4e83e6f300eed097b120e1cbb3d109
toandangkhac <toandang1704@gmail.com>
2024-06-11 21:19:38 +0700

    login phân quyền

--- app/src/main/java/com/example/kitchen_appliances_android_java/fragment/AccountFragment.java
+++ app/src/main/java/com/example/kitchen_appliances_android_java/fragment/AccountFragment.java
@@ -1,6 +1,8 @@
 package com.example.kitchen_appliances_android_java.fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -39,6 +41,10 @@ public class AccountFragment extends Fragment {
         // Set event
         btnLogout.setOnClickListener(v -> {
             // Logout
+            SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
             Intent intent = new Intent(getActivity(), LoginSignUpActivity.class);
             startActivity(intent);
             getActivity().finish();
